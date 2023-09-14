2e75c9d050db0fc39df9ff868e1fef5453cf92bc
Daniel Ilich <ukqfhf@yandex.ru>
2023-09-14 18:24:47 +0300

    feat: setup and basic structure

---
+++ .eleventy.js
@@ -0,0 +1,36 @@
+const CleanCSS = require("clean-css");
+const { minify } = require("terser");
+
+const setupJsminConfig = async function (code, callback) {
+  try {
+    const minified = await minify(code);
+    callback(null, minified.code);
+  } catch (err) {
+    console.error("Terser error: ", err);
+    // Fail gracefully.
+    callback(null, code);
+  }
+};
+
+const copyFilesConfig = {
+  "node_modules/modern-normalize/modern-normalize.css": "assets/styles",
+
+  "src/assets": "assets",
+};
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addFilter(
+    "cssmin",
+    (code) => new CleanCSS({}).minify(code).styles
+  );
+  eleventyConfig.addNunjucksAsyncFilter("jsmin", setupJsminConfig);
+  eleventyConfig.addPassthroughCopy(copyFilesConfig);
+  eleventyConfig.addWatchTarget("./src/assets");
+
+  return {
+    dir: {
+      input: "src",
+    },
+    templateFormats: ["html", "md"],
+  };
+};
---
+++ src/index.md
@@ -0,0 +1,15 @@
+---
+layout: layout.html
+---
+
+# Personal Website
+
+---
+
+Hey there
+
+I’m Daniel Ilich, a front-end web developer.
+
+I've been building stuff on the web for the last 4 years, working with clients across various industries.
+
+My focus is on creating engaging, accessible & performant interfaces for humans.
